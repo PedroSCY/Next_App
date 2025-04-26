b354355d511db87654d64eb8ad8efb0755e5702c
Pedro Lucas G. Prata <78231145+PedroSCY@users.noreply.github.com>
2025-04-26 12:06:01 -0300

    Usuario toggle

--- src/app/(interna)/shared/Cabecalho.tsx
+++ src/app/(interna)/shared/Cabecalho.tsx
@@ -1,6 +1,5 @@
 import { IconMenu2 } from '@tabler/icons-react'
 import React from 'react'
-import Menu from './Menu'
 import MenuUsuario from './MenuUsuario'
 
 export default function Cabecalho() {
@@ -9,7 +8,7 @@ export default function Cabecalho() {
         flex items-center justify-between px-7 h-20 bg-zinc-950
     '>
         <IconMenu2 className='text-zinc-400 hover:text-zinc-200 cursor-pointer'/>
-        <MenuUsuario/>
+        <MenuUsuario nome='Usuário Mock' email='usuario@zmail.com' imagem='https://picsum.photos/id/64/200'/>
     </header>
   )
 }
--- src/app/(interna)/shared/MenuUsuario.tsx
+++ src/app/(interna)/shared/MenuUsuario.tsx
@@ -1,7 +1,26 @@
-import React from 'react'
+import Image from "next/image";
+import React from "react";
 
-export default function MenuUsuario() {
+export interface MenuUsuarioProps {
+    nome: string
+    email: string
+    imagem: string
+}
+
+export default function MenuUsuario(props: MenuUsuarioProps) {
   return (
-    <div>Menu Usuario</div>
-  )
+    <div className="flex items-center gap-3 cursor-pointer hover:bg-zinc-800 p-2 rounded-lg ">
+        <div className="flex flex-col">
+            <span className="text-zinc-300 font-bold text-sm">{props.nome}</span>
+            <span className="text-zinc-500 text-xs">{props.email}</span>
+        </div>
+      <Image
+        src={props.imagem}
+        alt="imagem do usuario"
+        width={45}
+        height={45}
+        className="self-center rounded-full"
+      />
+    </div>
+  );
 }
